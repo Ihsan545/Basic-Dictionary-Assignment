f3cd178e4db1d65b6332761c6b252afa4475cfc4
anwar <Ihsan545>
2020-10-19 18:52:29 -0500

    test failed

---
+++ test_more_fun_with_collections/test_dict_membership.py
@@ -0,0 +1,19 @@
+import unittest
+from more_fun_with_collections import dict_membership
+
+
+class ScoreTestCase(unittest.TestCase):
+    def test_in_dict(self):
+        """ tests false is true"""
+        name = 'score1'
+        expected_result = {'score': 50}
+        self.assertFalse(expected_result.get(name))
+
+    def test_in_dict_is_true(self):
+        name = 'score3'
+        expected_result = {'score3': 0}
+        self.assertTrue(expected_result.get(name))
+
+
+if __name__ == '__main__':
+    unittest.main()
